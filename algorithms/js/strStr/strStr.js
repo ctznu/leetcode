f9319ec92ba770d12f3e70bb8af282e94f1369c3
ctznu <ctzn2010@qq.com>
2018-12-30 22:49:09 +0800

    Create strStr.js

---
+++ algorithms/js/strStr/strStr.js
@@ -0,0 +1,13 @@
+/*
+ * @Source: https://leetcode-cn.com/problems/implement-strstr/
+ * @Author: Tian Zhuo
+ * @Date: 2018-12-30 22:48:25
+ */
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle);
+};
